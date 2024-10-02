d6a9245cbc7d6b77271d37845c603f7d6d16f795
Daniel <daniel@daniels-MacBook-Pro.local>
2024-10-02 19:17:45 +0100

    Created routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,16 @@
+# I setup authentication following this tutorial: https://sdrmike.medium.com/rails-7-api-only-app-with-devise-and-jwt-for-authentication-1397211fb97c
+
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
